0851862cb200e3e9a8a30d144399db10611fcef3
Alec Mitrokostas <apmitrokostas@gmail.com>
2020-08-31 14:50:30 -0400

    replaced landing giv with mov

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
 import './App.css';
-import Boat from './4litreboat.gif';
+import Boat from './IMG_4603.mov';
 
 class Home extends Component {
     render(){
         return(
             <div className="home-wrapper">
-                <img src={Boat} alt="o.o"/>
+                <video className="boat-vid" src={Boat} autoPlay muted loop>Your browser does not support the video tag :(</video>
             </div>
         );
     }
